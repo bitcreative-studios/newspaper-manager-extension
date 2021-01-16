b44a8c364a912b96df77d79b6418627d207341ad
George Bockari <bitwhys@gmail.com>
2021-01-16 11:17:21 -0800

    feat(background): adds ability for user to interact with toolbar icon on appropriate pages

--- src/background/background.js
+++ src/background/background.js
@@ -2,6 +2,18 @@ const DEFAULT_COLOR = '#ff0844'
 
 chrome.runtime.onInstalled.addListener(function () {
   chrome.storage.sync.set({ color: DEFAULT_COLOR }, function () {
-    console.log(`The  color is ${DEFAULT_COLOR}`)
+    console.log(`The color is ${DEFAULT_COLOR}.`)
+  })
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
+    chrome.declarativeContent.onPageChanged.addRules([
+      {
+        conditions: [
+          new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: { hostEquals: 'www.open.online' },
+          }),
+        ],
+        actions: [new chrome.declarativeContent.ShowPageAction()],
+      },
+    ])
   })
 })
